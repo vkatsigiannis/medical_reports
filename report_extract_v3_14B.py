14ef520eba2f97706089717d730954a4eef3b457
vkatsigiannis <vassiliskat@gmail.com>
2026-03-19 08:51:51 +0000

    1.5B results for 728 samples. More fewshots added.

--- report_extract_v3_14B.py
+++ report_extract_v3_14B.py
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     # print(openai.Model.list()) # List all OpenAI models
 
     # ======================= CONFIG =======================
-    # MODEL_ID = "Qwen/Qwen2.5-1.5B-Instruct" 
+    MODEL_ID = "Qwen/Qwen2.5-1.5B-Instruct" 
     # MODEL_ID = "mistralai/Mistral-7B-Instruct-v0.3"
-    MODEL_ID = "Qwen/Qwen2.5-7B-Instruct"
+    # MODEL_ID = "Qwen/Qwen2.5-7B-Instruct"
     # MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct" # requires access
     # MODEL_ID = "Qwen/Qwen2.5-14B-Instruct"
     # MODEL_ID = "Qwen/Qwen2.5-32B-Instruct"
@@ -46,8 +46,8 @@ if __name__ == "__main__":
 
     # report_paths = os.listdir("txt/")[0:4]
     # report_paths = all_reports[667:668]
-    # report_paths = os.listdir("txt/")[0:]
-    report_paths = os.listdir("txt/")[709:]
+    report_paths = os.listdir("txt/")[0:]
+    # report_paths = os.listdir("txt/")[709:]
     # report_paths = os.listdir(r"txt/541_728/")[0:4]
     report_paths.sort()
     # report_path = report_paths[:50]
@@ -102,7 +102,7 @@ if __name__ == "__main__":
                 if attr not in ["report_text", "mass_gate", "nme_gate"]:
                     print(f"{attr}: {getattr(patient, attr)}")
             
-            patient.save_to_csv(ORDERED_FIELDS, csv_path="reports_extracted_test_728reports_7B.csv")
+            patient.save_to_csv(ORDERED_FIELDS, csv_path="reports_extracted_test_728reports_1B.csv")
             # patient.save_to_csv(ORDERED_FIELDS, csv_path="reports_extracted_temp.csv")
             # save_to_json_path = f"json/{pat_id}.json"
             # patient.save_to_json(ORDERED_FIELDS, json_path=save_to_json_path)
@@ -114,7 +114,7 @@ if __name__ == "__main__":
 
 
     df = lib.evaluate_categorical_metrics(
-    path_pred="reports_extracted_test_728reports_7B.csv",
+    path_pred="reports_extracted_test_728reports_1B.csv",
     path_gt="GT_gpt5_2_1.xlsx",
     metrics=("AccAll", "AccPresent", "AccNull", "GoldCoverage"),
     )
