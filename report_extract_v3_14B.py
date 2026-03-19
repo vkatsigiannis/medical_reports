4bc24c251ee0d551fe37877eac84facfcfaeae48
vkatsigiannis <vassiliskat@gmail.com>
2026-03-19 08:27:23 +0000

    7B results for 728 samples.

--- report_extract_v3_14B.py
+++ report_extract_v3_14B.py
@@ -46,7 +46,8 @@ if __name__ == "__main__":
 
     # report_paths = os.listdir("txt/")[0:4]
     # report_paths = all_reports[667:668]
-    report_paths = os.listdir("txt/")[0:]
+    # report_paths = os.listdir("txt/")[0:]
+    report_paths = os.listdir("txt/")[709:]
     # report_paths = os.listdir(r"txt/541_728/")[0:4]
     report_paths.sort()
     # report_path = report_paths[:50]
